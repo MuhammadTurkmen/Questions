fa035369086c98c3d40e0656c372978616231054
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-09 12:17:40 +0430

    init

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const btns = document.querySelectorAll('.question-btn')
 
 btns.forEach(function(btn) {
     btn.addEventListener('click', function(e) {
-        
+        const  question = e.currentTarget.parentElement.parentElement
     })
 })
