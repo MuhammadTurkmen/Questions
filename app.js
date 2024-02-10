9165faab3143ad7414f2fe6c9874ae17a7147fb8
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-10 14:14:40 +0430

    init

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 //using selectors inside the element
 // traversing the dom
-const btns = document.querySelectorAll('.question-btn')
 
-btns.forEach(function(btn) {
-    btn.addEventListener('click', function(e) {
-        const  question = e.currentTarget.parentElement.parentElement
-        question.classList.toggle('show-text')
-    })
-})     
+const questions = document.querySelector('.question')
+
+
+
+// const btns = document.querySelectorAll('.question-btn')
+
+// btns.forEach(function(btn) {
+//     btn.addEventListener('click', function(e) {
+//         const  question = e.currentTarget.parentElement.parentElement
+//         question.classList.toggle('show-text')
+//     })
+// })     
