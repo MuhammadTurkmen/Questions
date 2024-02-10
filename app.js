6d68c4251e8cc0fe9897d145c24e96f93a31fa54
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-10 14:21:15 +0430

    init

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 //using selectors inside the element
 // traversing the dom
 
-const questions = document.querySelector('.question')
+const question = document.querySelector(".question")
 
-questions.forEach(function (question) {
-    const btn = question.querySelector('.question-btn')
-    btn.addEventListner('click', function() {
-        question.cla 
-    })
-})
+question.forEach()
+
+
 
 
 // const btns = document.querySelectorAll('.question-btn')
